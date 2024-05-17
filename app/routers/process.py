e5f78d4abca067ce815ad79adffc877e33b4008a
Donna Mägi <magi.donna@gmail.com>
2024-05-17 18:57:48 +0200

    added route for generating keywords

--- app/routers/process.py
+++ app/routers/process.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
 from groq import Groq
 from dotenv import load_dotenv
 import os
@@ -13,16 +12,34 @@ client = Groq(
 
 router = APIRouter(prefix="/llama", tags=["llama"])
 
-class PromptRequest(BaseModel):
-  prompt: str
 
-@router.post("/chat")
-def chat_to_llama(request : PromptRequest):
+@router.post("/get_summary")
+def get_summary(content:str):
   chat_completion = client.chat.completions.create(
     messages=[
       {
         "role": "user",
-        "content": request.prompt,
+        "content": f"Summarize the text in 2-3 sentences. Be precise, no introduction needed: {content}",
+      }
+    ],
+    model="llama3-70b-8192",
+  )
+  return chat_completion.choices[0].message.content
+
+@router.post("/get_keywords")
+def get_keywords(summary:str):
+  chat_completion = client.chat.completions.create(
+    messages=[
+      {
+        "role": "user",
+        "content": 
+          f""" 
+            Generate 3-5 relevant keywords for an article's general topic. What areas of interest does the article cover?
+            Avoid repetition, and keep the keywords general. Use plural case if necessary.
+            This is the article's summary: {summary}.
+            Answer ONLY with the keywords. Every keyword should be 1-2 words only. 
+            Answer with a list of strings. Example format: ['keyword', 'keyword', 'keyword']
+          """,
       }
     ],
     model="llama3-70b-8192",
