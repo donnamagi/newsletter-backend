cf7b0cc1cf0282baf56f262782fe26cce9947d07
Donna Mägi <magi.donna@gmail.com>
2024-05-17 19:42:57 +0200

    scraping service logic

--- app/routers/scrape.py
+++ app/routers/scrape.py
@@ -1,8 +1,7 @@
 import requests
-import re
 from fastapi import APIRouter
 from bs4 import BeautifulSoup
-import re
+from services.scrape import clean_text
 
 router = APIRouter(tags=["scrape"])
 
@@ -12,42 +11,9 @@ def scrape_content(url: str):
   if content.status_code == 200:
     soup = BeautifulSoup(content.text, 'html.parser')
 
-    description = get_description(soup)
-    body = get_body(soup)
-    summary = description + clean_text(body)
+    summary = clean_text(soup)
 
     return summary
   else:
     return {"error": f"Error fetching content: {content.status_code}"}
 
-
-def clean_text(text):
-  """Remove extra spaces, newlines, and any script elements."""
-
-  text = re.sub('\s+', ' ', text)
-  text = re.sub('\n', ' ', text)
-  text = re.sub(r'<script.*?</script>', '', text, flags=re.DOTALL)
-
-  return text.strip()
-
-
-def get_description(soup: BeautifulSoup):
-  """Extract the meta description or og:description from the HTML."""
-
-  description = ''
-  meta_description = soup.find('meta', attrs={'name': 'description'})
-  og_description = soup.find('meta', attrs={'property': 'og:description'})
-  if meta_description:
-    description = meta_description.get('content', '')
-  elif og_description:
-    description = og_description.get('content', '')
-
-  return description
-
-
-def get_body(soup: BeautifulSoup):
-  """Extract the main body of text from the HTML."""
-
-  paragraphs = [p.get_text().strip() for p in soup.find_all('p')]
-  paragraph_text = ' '.join(paragraphs)
-  return paragraph_text[:3000]
---
+++ app/services/scrape.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import re
+
+def remove_chars(text):
+  """Remove extra spaces, newlines, and any script elements."""
+
+  text = re.sub('\s+', ' ', text)
+  text = re.sub('\n', ' ', text)
+  text = re.sub(r'<script.*?</script>', '', text, flags=re.DOTALL)
+
+  return text.strip()
+
+
+def get_description(soup: BeautifulSoup):
+  """Extract the meta description or og:description from the HTML."""
+
+  description = ''
+  meta_description = soup.find('meta', attrs={'name': 'description'})
+  og_description = soup.find('meta', attrs={'property': 'og:description'})
+  if meta_description:
+    description = meta_description.get('content', '')
+  elif og_description:
+    description = og_description.get('content', '')
+
+  return description
+
+
+def get_body(soup: BeautifulSoup):
+  """Extract the main body of text from the HTML."""
+
+  paragraphs = [p.get_text().strip() for p in soup.find_all('p')]
+  paragraph_text = ' '.join(paragraphs)
+  return paragraph_text[:3000]
+
+def clean_text(soup: BeautifulSoup):
+  """Main return function to clean the text."""
+    
+  description = get_description(soup)
+  body = get_body(soup)
+  summary = description + clean_text(body)
+  
+  return summary
